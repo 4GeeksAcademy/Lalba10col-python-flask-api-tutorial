d339fef057d63e2b2696e0db0540c8bb9985cdb5
Luis Angel Alba Malagon <lalba10@icloud.com>
2025-03-22 04:36:46 +0000

    Avance proyecto Flask 210325

---
+++ src/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+from flask import request
+
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def todos_list():
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body",request_body)
+    todos.append(request_body)  # Paso 2: Agregar el diccionario a la lista todos
+    return jsonify(todos)  # Paso 3: Devolver la lista actualizada
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Estas dos líneas siempre deben estar al final de tu archivo app.py
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
